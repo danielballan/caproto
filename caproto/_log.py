8e24a4e21ea8009a2d3daee000f2a00dce5d80dc
Maksim Rakitin <mrakitin@users.noreply.github.com>
2019-04-12 14:08:45 -0400

    Update caproto/_log.py
    Co-Authored-By: ke-zhang-rd <kezhang@bnl.gov>

--- caproto/_log.py
+++ caproto/_log.py
@@ -219,6 +219,7 @@ class PVFilter(logging.Filter):
         self.names = names
         self.level = level
         self.pv_unrelated_flag = pv_unrelated_flag
+
     def filter(self, record):
         if hasattr(record, 'pv'):
             for i in self.names:
